ca9189553c48467cab2de8824ee592154a499c86
Lithara Perera <litharaperera2002@gmail.com>
2023-04-26 16:01:00 +0530

    stage 01 completed

--- one-portfolio/src/components/Contact.jsx
+++ one-portfolio/src/components/Contact.jsx
@@ -4,7 +4,7 @@ import ContactImg from "../images/ContactImg.png";
 const Contact = () => {
   return (
     <div>
-        <div name="home" className="h-screen w-full right-0 mt-[2000px]">
+        <div name="home" className="h-screen w-full right-0 mt-[2000px] p-4">
 
             <div className="mx-auto flex flex-col px-4 md:flex-row w-4/5 bg-[#2D2D2D] right-0">
       
@@ -17,12 +17,16 @@ const Contact = () => {
                         Drop me a message below
                     </h1>
                     <div>
-
-                    </div>
-                    <div className="pb-10">
-                        <button className="text-[#2D2D2D] w-fit px-6 py-2 flex items-center bg-[#AA946B] text-center">
-                            <span className="text-lg md:text-xl cursor-pointer hover:scale-105 duration-200">Let's Work</span>
-                        </button>
+                        <form action="https://getform.io/f/5fc5ebf4-5afe-498b-8e2d-30c0d12c4836" method="POST" className="mt-5 flex flex-col">
+                            <input type="text" name="name" className="bg-transparent border border-[#AA946B] text-white text-[16px] md:text-[20px] py-2 px-4 my-2 focus:outline-none" placeholder="Enter your name"/>
+                            <input type="email" name="email" className="bg-transparent border border-[#AA946B] text-white text-[16px] md:text-[20px] py-2 px-4 my-2 focus:outline-none" placeholder="Enter your email"/>
+                            <textarea name="messsage" rows="10" className="bg-transparent border border-[#AA946B] text-white text-[16px] md:text-[20px] py-2 px-4 my-2 focus:outline-none" placeholder="Enter your message"></textarea>
+                            <div className="pt-8 pb-10">
+                                <button className="text-[#2D2D2D] w-fit px-6 py-2 flex items-center bg-[#AA946B] text-center">
+                                    <span className="text-lg md:text-xl cursor-pointer hover:scale-105 duration-200">Let's Work</span>
+                                </button>
+                            </div>
+                        </form>
                     </div>
                 </div>
 
